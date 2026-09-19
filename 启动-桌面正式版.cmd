bdf3e77e2e674ad211fca0b1a33b31a58b4d49f4
yty060525-commits <yty060525@gmail.com>
2026-09-19 10:13:45 +0800

    deploy: full PWA build

--- 启动-桌面正式版.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-rem 启动正式版(免安装便携 exe)。数据库自动创建在 exe 同目录的 data\ 下，随文件夹迁移。
-set "EXE=%~dp0release\windows\命理客户端.exe"
-if not exist "%EXE%" (
-  echo [ERROR] %EXE% 不存在，请先运行 client\build-windows.cmd 构建。
-  pause
-  exit /b 1
-)
-start "" "%EXE%"
